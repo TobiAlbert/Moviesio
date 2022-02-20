e4eda277faa15ec51eb29fea3d4ec05439987cb7
tobialbert <daadatobi@gmail.com>
2022-02-20 13:37:51 +0100

    Feat: Download list of popular movies from api endpoint.

--- app/src/main/java/com/tobidaada/movieio/di/RemoteModule.kt
+++ app/src/main/java/com/tobidaada/movieio/di/RemoteModule.kt
@@ -1,11 +1,16 @@
 package com.tobidaada.movieio.di
 
 import android.content.Context
-import com.readystatesoftware.chuck.ChuckInterceptor
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
+import com.tobidaada.movieio.BuildConfig
 import com.tobidaada.movieio.features.movies.data.datasource.remote.MovieService
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
@@ -14,6 +19,7 @@ import java.util.concurrent.TimeUnit
 import javax.inject.Singleton
 
 @Module
+@InstallIn(SingletonComponent::class)
 object RemoteModule {
 
     @Provides
@@ -23,7 +29,7 @@ object RemoteModule {
         val originalHttpUrl = request.url()
 
         val url = originalHttpUrl.newBuilder()
-            .addQueryParameter("appid", "BuildConfig.API_KEY")
+            .addQueryParameter("api_key", BuildConfig.API_KEY)
             .build()
 
         val builder = request.newBuilder()
@@ -37,14 +43,33 @@ object RemoteModule {
 
     @Provides
     @Singleton
+    fun provideChuckerInterceptor(
+        @ApplicationContext context: Context
+    ): ChuckerInterceptor {
+        val chuckerCollector = ChuckerCollector(
+            context = context,
+            showNotification = true,
+            retentionPeriod = RetentionManager.Period.ONE_HOUR
+        )
+
+        return ChuckerInterceptor.Builder(context)
+            .collector(chuckerCollector)
+            .maxContentLength(250_000L)
+            .redactHeaders("Auth-Token", "Bearer")
+            .alwaysReadResponseBody(true)
+            .build()
+    }
+
+    @Provides
+    @Singleton
     fun providesOkHttpClient(
-        @ApplicationContext context: Context,
-        interceptor: Interceptor
+        interceptor: Interceptor,
+        chuckerInterceptor: ChuckerInterceptor
     ): OkHttpClient =
         OkHttpClient.Builder()
             .connectTimeout(30, TimeUnit.SECONDS)
             .retryOnConnectionFailure(true)
-            .addInterceptor(ChuckInterceptor(context))
+            .addInterceptor(chuckerInterceptor)
             .addInterceptor(interceptor)
             .build()
 
