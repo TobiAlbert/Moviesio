44bc5600cd3b34945f7c4c5295486c6dec6fec25
tobialbert <daadatobi@gmail.com>
2022-02-21 05:48:46 +0100

    Fix: Add `Inject` annotation

--- app/src/main/java/com/tobidaada/movieio/utils/AppDispatchers.kt
+++ app/src/main/java/com/tobidaada/movieio/utils/AppDispatchers.kt
@@ -1,16 +1,16 @@
 package com.tobidaada.movieio.utils
 
 import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 import kotlin.coroutines.CoroutineContext
 
 interface AppDispatchers {
     fun io(): CoroutineContext
     fun main(): CoroutineContext
     fun unconfined(): CoroutineContext
-
 }
 
-class AppDispatchersImpl: AppDispatchers {
+class AppDispatchersImpl @Inject constructor(): AppDispatchers {
 
     override fun io(): CoroutineContext = Dispatchers.IO
 
