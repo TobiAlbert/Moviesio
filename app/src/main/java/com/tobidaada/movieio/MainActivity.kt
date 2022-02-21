44c27fdc013726cdb03f1b28c9fca3eb0b9f2012
tobialbert <daadatobi@gmail.com>
2022-02-21 10:52:33 +0100

    Feat: Setup navigation to move from "movie list" to "movie" screen.

--- app/src/main/java/com/tobidaada/movieio/MainActivity.kt
+++ app/src/main/java/com/tobidaada/movieio/MainActivity.kt
@@ -2,6 +2,10 @@ package com.tobidaada.movieio
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
+import com.tobidaada.movieio.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,6 +13,14 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // configure app bar with
+        val navController = (supportFragmentManager
+            .findFragmentById(binding.navHostFragment.id) as NavHostFragment)
+            .navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
     }
 }
--- app/src/main/java/com/tobidaada/movieio/features/movies/presentation/ui/MovieListFragment.kt
+++ app/src/main/java/com/tobidaada/movieio/features/movies/presentation/ui/MovieListFragment.kt
@@ -1,19 +1,23 @@
 package com.tobidaada.movieio.features.movies.presentation.ui
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import com.tobidaada.movieio.R
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.tobidaada.movieio.databinding.FragmentMovieListBinding
 import com.tobidaada.movieio.features.movies.presentation.MovieViewModel
 import com.tobidaada.movieio.features.movies.presentation.models.GetMoviesUiState
+import com.tobidaada.movieio.features.movies.presentation.ui.adapters.MovieAdapter
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
@@ -23,6 +27,7 @@ class MovieListFragment : Fragment() {
 
     private val movieViewModel: MovieViewModel by viewModels()
     private lateinit var binding: FragmentMovieListBinding
+    private lateinit var movieAdapter: MovieAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,19 +40,41 @@ class MovieListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setupUi()
+
         viewLifecycleOwner.lifecycleScope.launch {
             viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.RESUMED) {
                 movieViewModel.state.collect {
-                    val message = when(it) {
-                        is GetMoviesUiState.LoadingState -> "Loading"
-                        is GetMoviesUiState.ErrorState -> "An Error Occurred: ${it.message}"
-                        is GetMoviesUiState.SuccessState -> "On Success: ${it.data}"
-                    }
+                    when (it) {
+                        is GetMoviesUiState.LoadingState -> {
 
-                    Log.i("MainActivity", message)
+                        }
+                        is GetMoviesUiState.ErrorState -> {
+                            if (it.message.isNotEmpty()) {
+                                Toast.makeText(requireContext(), it.message, Toast.LENGTH_SHORT)
+                                    .show()
+                            }
+                        }
+                        is GetMoviesUiState.SuccessState -> movieAdapter.submitList(it.data)
+                    }
                 }
             }
         }
     }
 
+    private fun setupUi() {
+        movieAdapter = MovieAdapter(::onMovieItemSelected)
+        binding.movieRv.apply {
+            adapter = movieAdapter
+            layoutManager = LinearLayoutManager(requireContext())
+            addItemDecoration(DividerItemDecoration(requireContext(), RecyclerView.VERTICAL))
+        }
+    }
+
+    private fun onMovieItemSelected(movieId: Int) {
+        val action = MovieListFragmentDirections
+            .actionMovieListFragmentToMovieFragment(movieId)
+
+        findNavController().navigate(action)
+    }
 }
